b5588e189071f34444e6056805583df571cba17c
Rambo <873463678@qq.com>
2021-05-12 18:01:09 +0800

    配置了退出登录接口

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,17 +1,52 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
+from apps.users.forms import LoginForm, DynamicLoginForm
+
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        # 重定向跳转，转首页配置"index"，转登录界面配置"login"
+        return HttpResponseRedirect(reverse("login"))
 
 
 class LoginView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "login.html")
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("index"))
+
+        login_form = DynamicLoginForm()
+        return render(request, "login.html", {
+            "login_form": login_form
+        })
 
     def post(self, request, *args, **kwargs):
-        user_name = request.POST.get("username", "")
-        password = request.POST.get("password", "")
+        # 使用表单验证可简化
+        login_form = LoginForm(request.POST)
+
+        if login_form.is_valid():
+            # 用于通过用户和密码查询用户是否存在
+            user_name = login_form.cleaned_data["username"]
+            password = login_form.cleaned_data["password"]
+            user = authenticate(username=user_name, password=password)
+            # 1.通过用户名查询到用户
+            # 2.需要先加密再通过加密之后的密码查询
+            # from apps.users.models import UserProfile
+            # user = UserProfile.objects.get(username=user_name, password=password)
+            if user is not None:
+                # 查询到用户
+                login(request, user)
+                # 登陆成功之后该如何返回页面
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                # 未查询到用户
+                return render(request, "login.html", {"msg": "用户名或密码错误", "login_form": login_form})
+        else:
+            return render(request, "login.html", {"login_form": login_form})
 
         # 验证规则过于冗余繁琐
         # if not user_name:
@@ -20,23 +55,3 @@ class LoginView(View):
         #     return render(request, "login.html", {"msg": "请输入密码"})
         # if len(password) < 3:
         #     return render(request, "login.html", {"msg": "密码格式不正确"})
-
-        # 使用表单验证可简化
-
-        # 用于通过用户和密码查询用户是否存在
-        user = authenticate(username=user_name, password=password)
-
-        # 1.通过用户名查询到用户
-        # 2.需要先加密再通过加密之后的密码查询
-        # from apps.users.models import UserProfile
-        # user = UserProfile.objects.get(username=user_name, password=password)
-
-        if user is not None:
-            # 查询到用户
-            login(request, user)
-            # 登陆成功之后该如何返回页面
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            # 未查询到用户
-            return render(request, "login.html", {"msg": "用户名或密码错误"})
-
--- ram_mooc/urls.py
+++ ram_mooc/urls.py
@@ -13,26 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+# from django.contrib import admin
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 import xadmin
-# xadmin.autodiscover()
-
-# from xadmin.plugins import xversion
-# xversion.register_models()
-
-from apps.users.views import LoginView
+from apps.users.views import LoginView, LogoutView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
+    path('captcha/', include('captcha.urls')),
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
     path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
     path('register/', TemplateView.as_view(template_name="register.html"), name="register"),
 ]
-
 
 # 1.CBV(class base view)CBV模式有利于代码的重用，class可继承
 # 2.FBV(function base view)
